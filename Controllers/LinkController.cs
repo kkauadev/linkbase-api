fb85c457c2a1ac97b0dd4251d22237bfa51b87ba
Kato2004 <kauaalvesnascimento22@gmail.com>
2024-01-01 12:43:51 -0300

    standardized link controller return

---
+++ Controllers/LinkController.cs
@@ -0,0 +1,40 @@
+﻿using LinkBaseApi.Context;
+using LinkBaseApi.DTOs;
+using LinkBaseApi.Models;
+using LinkBaseApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkBaseApi.Controllers
+{
+  public class LinkController (ILogger<LinkController> logger, DataContext dataContext, ValidationService validationService) : ControllerBase
+  {
+    public readonly ILogger _logger = logger;
+    public readonly DataContext _dataContext = dataContext;
+    public readonly ValidationService _validationService = validationService;
+
+    [HttpPost("/link/{id}")]
+    public async Task<ActionResult<LinkDTOView>> CreateLink(string id, [FromBody] LinkDTO linkDTO)
+    {
+      if (!Guid.TryParse(id, out Guid folderId))
+      {
+        return BadRequest("Insira um ID válido");
+      }
+
+      if (_dataContext.Links.Any(l => l.Title == linkDTO.Title)) { }
+
+      Link link = new()
+      {
+        FolderId = folderId,
+        Description = linkDTO.Description,
+        Title = linkDTO.Title,
+        Url = linkDTO.Url
+      };
+
+      _dataContext.Add(link);
+      await _dataContext.SaveChangesAsync();
+
+      return Ok(link);
+    }
+  }
+
+}
--- Models/Link.cs
+++ Models/Link.cs
@@ -2,11 +2,11 @@
 {
   public class Link
   {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public Guid FolderId { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string Url { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public required string Url { get; set; }
     public DateTime Created { get; set; }
     public DateTime LastUpdated { get; set; }
   }
