8f14a4424533c4ab0e33af98298c2b7e55f4ceb2
Kato2004 <kauaalvesnascimento22@gmail.com>
2024-01-24 22:50:16 -0300

    bug: resolving error from incorrect return from metod

--- LinkBaseApi.Domain/Interfaces/IFolderRepository.cs
+++ LinkBaseApi.Domain/Interfaces/IFolderRepository.cs
@@ -4,6 +4,6 @@ namespace LinkBaseApi.Domain.Interfaces
 {
   public interface IFolderRepository : IBaseRepository<Folder>
   {
-    Task<Folder?> GetByAuthor(Guid authorId, CancellationToken cancellationToken);
+    Task<ICollection<Folder>> GetByAuthor(Guid authorId, CancellationToken cancellationToken);
   }
 }
