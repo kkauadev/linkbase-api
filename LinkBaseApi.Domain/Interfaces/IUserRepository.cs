325fc1b81a89cd1e473f215a94515359eecd014e
Kato2004 <kauaalvesnascimento22@gmail.com>
2024-01-24 22:49:22 -0300

    feat: adding another interface to get user by email or username

--- LinkBaseApi.Domain/Interfaces/IUserRepository.cs
+++ LinkBaseApi.Domain/Interfaces/IUserRepository.cs
@@ -2,10 +2,11 @@
 
 namespace LinkBaseApi.Domain.Interfaces
 {
-  public interface IUserRepository : IBaseRepository<User>
-  {
-    Task<User?> GetByUsername(string username, CancellationToken cancellationToken);
-    Task<User?> GetById(Guid id, CancellationToken cancellationToken);
-    Task<List<User>> GetAllWithFolders(CancellationToken cancellationToken);
-  }
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<User?> GetByUsername(string username, CancellationToken cancellationToken);
+        Task<User?> GetById(Guid id, CancellationToken cancellationToken);
+        Task<List<User>> GetAllWithFolders(CancellationToken cancellationToken);
+        Task<User?> GetByEmailOrUsername(string email, string username, CancellationToken cancellationToken);
+    }
 }
