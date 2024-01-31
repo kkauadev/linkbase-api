6667b12d1da4cec1e80a96a0c7e8a0326ee1892b
Kato2004 <kauaalvesnascimento22@gmail.com>
2024-01-31 10:18:57 -0300

    feat: add two new controllers to get all created links and all created links from a folder

--- LinkBaseApi.API/Controllers/LinkController.cs
+++ LinkBaseApi.API/Controllers/LinkController.cs
@@ -1,4 +1,8 @@
-﻿using LinkBaseApi.Application.UseCases.Links.CreateLink;
+﻿using LinkBaseApi.Application.DTOs;
+using LinkBaseApi.Application.UseCases.Links.CreateLink;
+using LinkBaseApi.Application.UseCases.Links.DeleteLink;
+using LinkBaseApi.Application.UseCases.Links.GetAllLinks;
+using LinkBaseApi.Application.UseCases.Links.GetLinkByFolder;
 using LinkBaseApi.Application.Wrappers;
 using LinkBaseApi.DTOs;
 using MediatR;
@@ -6,12 +10,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LinkBaseApi.Controllers
 {
-	public class LinkController(ILogger<LinkController> logger, IMediator mediator) : ControllerBase
+    public class LinkController(ILogger<LinkController> logger, IMediator mediator) : ControllerBase
     {
         private readonly IMediator _mediator = mediator;
         private readonly ILogger _logger = logger;
 
-        [HttpPost("folder/link/{folderId}")]
+        [HttpGet("links")]
+        public async Task<ActionResult<string>> GetAllLinks(CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(new GetLinksRequest(), cancellationToken);
+
+            return Ok(response);
+        }
+
+        [HttpGet("links/{folderId}")]
+        public async Task<ActionResult<Response<List<LinkResponse>>>> GetLinksByFolder(Guid folderId, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(new GetLinkByFolderRequest { FolderId = folderId }, cancellationToken);
+
+            return Ok(response);
+        }
+
+		[HttpPost("folder/link/{folderId}")]
         public async Task<ActionResult<Response<CreateLinkResponse>>> CreateLink(Guid folderId, [FromBody] CreateLinkDTO linkDTO, CancellationToken cancellationToken)
         {
             var linkRequest = new CreateLinkRequest()
@@ -23,6 +43,14 @@ namespace LinkBaseApi.Controllers
             };
 
             var response = await _mediator.Send(linkRequest, cancellationToken);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("link/{id}")]
+        public async Task<ActionResult<Response<Guid>>> DeleteLink(Guid id, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(new DeleteLinkRequest { Id = id }, cancellationToken);
 
             return Ok(response);
         }
