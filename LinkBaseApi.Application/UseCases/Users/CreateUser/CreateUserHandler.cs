5e4f547c6e5884f89f06ebc8ee68a2629b50d16c
Kato2004 <kauaalvesnascimento22@gmail.com>
2024-01-31 10:12:16 -0300

    feat: add passwordHashService to encrypt password on create new user

--- LinkBaseApi.Application/UseCases/Users/CreateUser/CreateUserHandler.cs
+++ LinkBaseApi.Application/UseCases/Users/CreateUser/CreateUserHandler.cs
@@ -8,12 +8,13 @@ using MediatR;
 namespace LinkBaseApi.Application.UseCases.Users.CreateUser
 {
 	public class CreateUserHandler
-		(IUnitOfWork unitOfWork, IUserRepository userRepository, IMapper mapper)
+		(IUnitOfWork unitOfWork, IUserRepository userRepository, IMapper mapper, IPasswordHashService passwordHashService)
 			: IRequestHandler<CreateUserRequest, Response<Guid>>
 	{
 		private readonly IUnitOfWork _unitOfWork = unitOfWork;
 		private readonly IUserRepository _userRepository = userRepository;
 		private readonly IMapper _mapper = mapper;
+		private readonly IPasswordHashService _passwordHashService = passwordHashService;
 		public async Task<Response<Guid>> Handle
 			(CreateUserRequest request, CancellationToken cancellationToken)
 		{
@@ -22,6 +23,8 @@ namespace LinkBaseApi.Application.UseCases.Users.CreateUser
 			if (await _userRepository
 				.GetByEmailOrUsername(user.Email, user.Username, cancellationToken) != null)
 					throw new ApiException("User already exits");
+
+			user.Password = _passwordHashService.HashPassword(user.Password);
 
 			_userRepository.Create(user);
 
