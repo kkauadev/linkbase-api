e6d28498be67fe5e9e2117c349b9a57d4aeaa79e
Kato2004 <kauaalvesnascimento22@gmail.com>
2024-02-10 18:01:45 -0300

    feat: add authentication service interface in the application domain

---
+++ LinkBaseApi.Domain/Interfaces/IAuthenticationServices.cs
@@ -0,0 +1,8 @@
+﻿namespace LinkBaseApi.Domain.Interfaces
+{
+	public interface IAuthenticationService
+	{
+		string GenerateToken(Guid? userId, string email);
+		Task<(bool, Guid?)> ValidateCredentials(string username, string password, CancellationToken cancellationToken);
+	}
+}
