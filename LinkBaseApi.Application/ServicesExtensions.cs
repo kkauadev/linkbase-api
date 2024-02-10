52eb57ebe5bc6d436a4de6a0720df58aaaf45f6e
Kato2004 <kauaalvesnascimento22@gmail.com>
2024-02-10 17:50:46 -0300

    feat: add AuthenticationService and changing places

---
+++ LinkBaseApi.Application/ServicesExtensions.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using LinkBaseApi.Application.Services;
+using LinkBaseApi.Application.Shared.Behavior;
+using LinkBaseApi.Domain.Interfaces;
+using LinkBaseApi.Infrastructure.Services;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace LinkBaseApi.Application
+{
+    public static class ServicesExtensions
+    {
+        public static void ConfigureApplicationApp(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+			services.AddScoped<IPasswordHashService, PasswordHashService>();
+		}
+	}
+}
