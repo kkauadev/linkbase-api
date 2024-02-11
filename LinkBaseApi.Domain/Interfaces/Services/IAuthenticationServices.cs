ffaa844934b71f8d36135a5ebdd8cb7afb66a0a7
Kato2004 <kauaalvesnascimento22@gmail.com>
2024-02-10 23:55:16 -0300

    refactor: improve interfaces folder structure

---
+++ LinkBaseApi.Domain/Interfaces/Services/IAuthenticationServices.cs
@@ -0,0 +1,8 @@
+﻿namespace LinkBaseApi.Domain.Interfaces
+{
+	public interface IAuthenticationService
+	{
+		string GenerateToken(Guid userId, string email);
+		Task<Guid?> ValidateCredentials(string username, string password, CancellationToken cancellationToken);
+	}
+}
